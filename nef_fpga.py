5cebdaa76c068369743f09e3a3b20584acdc6b10
bmorcos <morcos.ben@gmail.com>
2020-02-07 16:23:37 -0500

    FPGA performance now matches sw ref
    Padded input stream. Initial FPGA outputs were zero
    so there may be some latency concerns with FPGA that
    skewed results. Haven't confirmed input-by-input
    compared to sw reference yet, but qualitative performance
    is now matched! Need to go over NengoFPGA communications
    and confirm where delays are and how to mitigate them
    to match sw version.

--- nef_fpga.py
+++ nef_fpga.py
@@ -1,5 +1,7 @@
 import pickle
 import nengo
+import argparse
+import logging
 
 import numpy as np
 
@@ -41,11 +43,15 @@ fpga_1 = 'pynq'
 
 # Flag for easily switching between software and hardware implementations
 # 0 = layer_0 on fpga; 1 = layer_1 on fpga; 2 = both; else all software
-USE_FPGA = -1
+parser = argparse.ArgumentParser()
+parser.add_argument("--fpga", type=int, default=-1)
+args = parser.parse_args()
+USE_FPGA = args.fpga
 
 nengo.rc.set("precision", "bits", "32")
+# logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
-with nengo.Network() as net:
+with nengo.Network(seed=1) as net:
     net.config[nengo.Connection].synapse = None
 
     inp = nengo.Node(size_in=inp_dim)
@@ -64,14 +70,18 @@ with nengo.Network() as net:
         fpga_0_ens.ensemble.normalize_encoders = False
         fpga_0_ens.ensemble.gain = np.ones(n_neurons)
         fpga_0_ens.connection.solver = NoSolver(np.eye(n_neurons))
-        fpga_0_ens.connection.transform = W_layer1.T
+        # fpga_0_ens.connection.transform = W_layer1.T
         fpga_0_ens.connection.synapse = None
+
+        l0_p = nengo.Probe(fpga_0_ens.output)
     else:
         layer_0 = nengo.Ensemble(n_neurons=n_neurons, dimensions=n_neurons,
                                  gain=np.ones(n_neurons),
                                  encoders=np.eye(n_neurons),
                                  normalize_encoders=False,
                                  bias=b_layer0, neuron_type=neuron_type)
+
+        l0_p = nengo.Probe(layer_0)
 
     if USE_FPGA in [1, 2]:
         # Create FPGA ens for layer 1
@@ -89,23 +99,34 @@ with nengo.Network() as net:
         fpga_1_ens.connection.solver = NoSolver(np.eye(n_neurons))
         # fpga_1_ens.connection.transform = W_output.T
         fpga_1_ens.connection.synapse = None
+
+        l1_p = nengo.Probe(fpga_1_ens.output)
     else:
         layer_1 = nengo.Ensemble(n_neurons=n_neurons, dimensions=n_neurons,
                                  gain=np.ones(n_neurons),
                                  encoders=np.eye(n_neurons),
                                  normalize_encoders=False,
                                  bias=b_layer1, neuron_type=neuron_type)
+        l1_p = nengo.Probe(layer_1)
 
     out_bias = nengo.Node(1)
 
     out = nengo.Node(size_in=out_dim)
 
     if USE_FPGA == 0:
-        nengo.Connection(inp, fpga_0_ens.input, transform=W_layer0.T)
-        nengo.Connection(fpga_0_ens.output, layer_1)
+        inp_conn = nengo.Connection(inp, fpga_0_ens.input, transform=W_layer0.T)
+        l0_conn = nengo.Connection(fpga_0_ens.output, layer_1,
+                         solver=NoSolver(np.eye(n_neurons)),
+                         transform=W_layer1.T)
         nengo.Connection(layer_1, out,
                          solver=NoSolver(np.eye(n_neurons)),
                          transform=W_output.T)
+
+        l0_in = nengo.Probe(fpga_0_ens.input)
+        l0_out = nengo.Probe(fpga_0_ens.output)
+        l01_dec = nengo.Probe(l0_conn, 'weights')
+        l01_in = nengo.Probe(l0_conn, 'input')
+        l01_out = nengo.Probe(l0_conn, 'output')
 
     elif USE_FPGA == 1:
         nengo.Connection(inp, layer_0, transform=W_layer0.T)
@@ -122,12 +143,18 @@ with nengo.Network() as net:
         nengo.Connection(fpga_1_ens.output, out, transform=W_output.T)
 
     else:
-        nengo.Connection(inp, layer_0, transform=W_layer0.T)
-        nengo.Connection(layer_0, layer_1,
+        inp_conn = nengo.Connection(inp, layer_0, transform=W_layer0.T)
+        l0_conn = nengo.Connection(layer_0, layer_1,
                          solver=NoSolver(np.eye(n_neurons)),
                          transform=W_layer1.T)
-        nengo.Connection(layer_1, out, solver=NoSolver(np.eye(n_neurons)),
+        l1_conn = nengo.Connection(layer_1, out, solver=NoSolver(np.eye(n_neurons)),
                          transform=W_output.T)
+
+        l0_in = nengo.Probe(layer_0, 'input')
+        l0_out = nengo.Probe(layer_0, 'decoded_output')
+        l01_dec = nengo.Probe(l0_conn, 'weights')
+        l01_in = nengo.Probe(l0_conn, 'input')
+        l01_out = nengo.Probe(l0_conn, 'output')
 
     nengo.Connection(out_bias, out, transform=np.expand_dims(b_output, axis=1))
 
@@ -147,28 +174,38 @@ stats = {
 
 # Precompute inputs so we can a single simulator,
 # it's slow to spin up a simulator on the FPGA
+# First and last inputs are doubled to allow for the FPGA latency/startup?
+# Not sure if we need padded final input or not, (in theory we do)
+# have not confirmed, though it is functional with only doubled first input
 all_inputs = []
 for features, text in test_data:
     if len(all_inputs) == 0:
-        all_inputs = np.squeeze(features)
+        all_inputs = np.vstack((np.squeeze(features)[0],
+                                np.squeeze(features),
+                                np.squeeze(features)[-1]))
     else:
-        all_inputs = np.vstack((all_inputs, np.squeeze(features)))
+        all_inputs = np.vstack((all_inputs,
+                                np.squeeze(features)[0],
+                                np.squeeze(features),
+                                np.squeeze(features)[-1]))
 
 feed = create_stream(all_inputs)
 net.inp.output = feed
 
 idx = 0
+sample = 0
 with nengo_fpga.Simulator(net) as sim:
     for features, text in test_data:
         inputs = np.squeeze(features)
-        n_steps = inputs.shape[0]
+        n_steps = inputs.shape[0] + 2  # Padded inputs
 
         sim.run_steps(n_steps)
 
-        results = sim.data[probe][idx:]
+        results = sim.data[probe][idx + 1:]  # Ignore first output from FPGA
         ids = np.argmax(results, axis=1)
         chars = [id_to_char[x] for x in ids]
 
+        print("Sample %d, (idx %d)" % (sample, idx))
         predicted_chars = merge(merge(''.join(chars)))
         print('Correct: ', text)
         print('Predicted: ', predicted_chars)
@@ -188,6 +225,14 @@ with nengo_fpga.Simulator(net) as sim:
                 stats["tn"] += 1
 
         idx += n_steps
+        sample += 1
+
+print("FPGA?", USE_FPGA)
+# print("L0-in", np.mean(sim.data[l0_in]), sim.data[l0_in].shape)
+# print("L0-out", np.mean(sim.data[l0_out][1:]), sim.data[l0_out].shape)
+# print("L01-dec", np.mean(sim.data[l01_dec][1:]), sim.data[l01_dec].shape)
+# print("L01-in", np.mean(sim.data[l01_in]), sim.data[l01_in].shape)
+# print("L01-out", np.mean(sim.data[l01_out][1:]), sim.data[l01_out].shape)
 
 print("Summary")
 print("=======")
